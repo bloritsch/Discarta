ddc1d16b6eac65f55f7f66d558244e0fe04a4f02
Berin Loritsch <bloritsch@d-haven.org>
2016-05-23 19:14:12 -0400

    Add project files.

---
+++ OfflneMap/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfflneMap
+{
+    public class Class1
+    {
+    }
+}
