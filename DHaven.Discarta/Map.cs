dc3e07adcff6c9497c172f59660e92a94dda8253
Berin Loritsch <bloritsch@d-haven.org>
2016-05-30 12:42:31 -0400

    Add licence headers.

--- DHaven.Discarta/Map.cs
+++ DHaven.Discarta/Map.cs
@@ -1,4 +1,19 @@
-﻿using DHaven.DisCarta.Internals;
+﻿#region Copyright 2016 D-Haven.org
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+
+using DHaven.DisCarta.Internals;
 using DHaven.DisCarta.Projections;
 using System.Collections.Specialized;
 using System.Windows;
@@ -11,19 +26,13 @@ namespace DHaven.DisCarta
     public class Map : Panel
     {
         public static readonly DependencyProperty ProjectionProperty = DependencyProperty.RegisterAttached("Projection", typeof(IProjection), typeof(Map), new FrameworkPropertyMetadata(new PseudoMercatorProjection(), FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange));
-        public static readonly DependencyProperty VisualExtentProperty = DependencyProperty.RegisterAttached("VisualExtentProperty", typeof(VisualExtent), typeof(Map), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange));
+        public static readonly DependencyProperty VisualExtentProperty = DependencyProperty.RegisterAttached("VisualExtentProperty", typeof(VisualExtent), typeof(Map), new FrameworkPropertyMetadata(new VisualExtent(), FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsArrange));
 
         private TranslateTransform mapOffset = new TranslateTransform();
 
         public Map()
         {
             LayoutTransform = mapOffset;
-            VisualExtent = new VisualExtent
-            {
-                Extent = Projection.World,
-                ZoomLevel = 0,
-                Screen = new Size()
-            };
         }
 
         public IProjection Projection
